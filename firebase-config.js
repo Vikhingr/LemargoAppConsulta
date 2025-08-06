86c01ef5e787a5fc52f3dd8fe47eb8fb6f8c29fe
Vikhingr <140551084+Vikhingr@users.noreply.github.com>
2025-08-05 23:05:39 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCCW3wmQ8sGjsiehPWI65JIEUaaej9VXQ",
+  authDomain: "lemargo-app-de-consulta.firebaseapp.com",
+  projectId: "lemargo-app-de-consulta",
+  storageBucket: "lemargo-app-de-consulta.firebasestorage.app",
+  messagingSenderId: "120054139227",
+  appId: "1:120054139227:web:531983ede51253e35ef27e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
